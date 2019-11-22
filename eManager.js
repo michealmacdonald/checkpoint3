64d953d40e4e0bc083222d501a84948462253e05
michealmacdonald <mikeycmacdonald@gmail.com>
2019-11-22 16:49:13 -0700

    Add files via upload

---
+++ eManager.js
@@ -0,0 +1,55 @@
+var spideyInfo = [{
+    name: "Peter Parker",
+    phone: "1231231234",
+    email: "webslinger@gmail.com",
+    title: "Spider-Man"
+}]
+var docOcInfo = [{
+    name: "Dr. Otto Octavius",
+    phone: "1231231234",
+    email: "tentacles@gmail.com",
+    title: "Doctor Octopus"
+}]
+var goblinInfo = [{
+    name: "Norman Osbourne",
+    phone: "1231231234",
+    email: "pumkinspicebomb@gmail.com",
+    title: "Green Goblin"
+}]
+var venomInfo = [{
+    name: "Eddie Brock",
+    phone: "1231231234",
+    email: "symbiote32@gmail.com",
+    title: "Venom"
+}]
+var shockerInfo = [{
+    name: "Herman Schultz",
+    phone: "1231231234",
+    email: "alwayseasytobeat@gmail.com",
+    title: "The Shocker"
+}]
+var managerObjects = {} 
+module.exports = {
+    beforeEach: browser => {
+        managerObjects = browser.page.eManagerObj()
+        managerObjects
+            .navigate()
+    },
+    after: browser => {
+        managerObjects.end()
+    },
+    'Spiderman Test': browser => {
+        spideyInfo.forEach(test => {
+            managerObjects
+            .spiderman(test)
+        })
+    },
+    'Goblin Test': browser => {
+        goblinInfo.forEach(test => {
+            managerObjects
+            .greenGoblin(test)
+        })
+
+    }
+}
+            
---
+++ eManagerObj.js
@@ -0,0 +1,100 @@
+var commands = {
+    spiderman: function (spideyInfo){
+        this
+        .click('@newEmployee')
+        .click('@name')
+        .clearValue('@name')
+        .setValue('@name', spideyInfo.name)
+        .clearValue('@phone')
+        .setValue('@phone', spideyInfo.phone)
+        .clearValue('@email')
+        .setValue('@email', spideyInfo.email)
+        .clearValue('@title')
+        .setValue('@title', spideyInfo.title)
+        .click('@save')
+        .click('@addEmployee')
+        .click('@spiderman')
+        .verify.valueContains('@name', spideyInfo.name)
+        .verify.valueContains('@phone',spideyInfo.phone)
+        .verify.valueContains('@email', spideyInfo.email)
+        .verify.valueContains('@title', "Spider-Man")
+        .click('@delete')
+        .api.acceptAlert()
+        .waitForElementNotPresent('@greenGoblin')
+        return this 
+    },    
+    greenGoblin: function (goblinInfo){
+        this
+        .click('@newEmployee')
+        .click('@name')
+        .clearValue('@name')
+        .setValue('@name', goblinInfo.name)
+        .clearValue('@phone')
+        .setValue('@phone', goblinInfo.phone)
+        .clearValue('@email')
+        .setValue('@email', goblinInfo.email)
+        .clearValue('@title')
+        .setValue('@title', goblinInfo.title)
+        .click('@save')
+        .click('@addEmployee')
+        .click('@greenGoblin')
+        .verify.valueContains('@name', goblinInfo.name)
+        .verify.valueContains('@phone', goblinInfo.phone)
+        .verify.valueContains('@email', goblinInfo.email)
+        .verify.valueContains('@title', goblinInfo.title)
+        .click('@delete')
+        .api.acceptAlert()
+        .waitForElementNotPresent('@greenGoblin')
+        return this
+    }
+}    
+module.exports = {
+    url: 'https://devmountain-qa.github.io/employee-manager-v2/build/index.html',
+    commands: [commands],
+     elements: {
+        spiderman:{
+            selector: '//li[text()="Peter Parker"]',
+            locateStrategy: 'xpath'
+        },
+        greenGoblin:{
+            selector: '//li[text()="Norman Osbourne"]',
+            locateStrategy: 'xpath'
+        },
+        addEmployee:{
+            selector: '//li[@name="addEmployee"]',
+            locateStrategy: 'xpath'
+        },
+        newEmployee:{
+            selector: '//li[text()="New Employee"]',
+            locateStrategy: 'xpath'
+        },
+        name:{
+            selector: '//input[@name="nameEntry"]',
+            locateStrategy: 'xpath'
+        },
+        phone:{
+            selector: '//input[@name="phoneEntry"]',
+            locateStrategy: 'xpath'
+        },
+        email:{
+            selector: '//input[@name="emailEntry"]',
+            locateStrategy: 'xpath'
+        },
+        title:{
+            selector: '//input[@name="titleEntry"]',
+            locateStrategy: 'xpath'
+        },
+        save:{
+            selector: '//button[@name="save"]',
+            locateStrategy: 'xpath'
+        },
+        cancel:{
+            selector: '//button[@name="cancel"]',
+            locateStrategy: 'xpath'
+        },
+        delete:{
+            selector: '//button[@name="delete"]',
+            locateStrategy: 'xpath'
+        }
+    }
+}
